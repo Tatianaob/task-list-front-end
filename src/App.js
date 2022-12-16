5d6e9b241e6a098299e3b049d93566bcf3a2ac28
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2022-12-16 13:13:56 -0800

    working on wave two

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,40 @@ const TASKS = [
 ];
 
 const App = () => {
+  const initialCopy = TASKS.map(task => {
+    return {...task};
+  });
+
+  const [tasksList, setTasksList] = useState(initialCopy);
+
+  const updateIsComplete = (taskId, updatedTask) => {
+    console.log('updateIsComplete is logged');
+    ///logic to update the taskList everytime the user click the task
+    const newTasksList = [];
+    for(const task of tasksList) {
+      if(task.id !== taskId) {
+        newTasksList.push(task);
+      }else{
+        const newTask = {
+          ...task,
+          isComplete: updatedTask
+        };
+      console.log('task.isComplete', task.isComplete);
+      newTasksList.push(newTask);
+      }
+    }
+    setTasksList(newTasksList);
+
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={TASKS} updateIsComplete={updateIsComplete} />}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
+const Task = ({ id, title, isComplete, updateIsComplete }) => {
   const [complete, setComplete] = useState(isComplete);
   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+  
+  const clickTask = (id) => {
+    console.log('inside the clickTask function');
+
+    updateIsComplete(id, !isComplete);
+    setComplete(!complete);
+  };
+
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        // onClick={() => setComplete(!complete)}
+        onClick={() => clickTask(id)}
       >
         {title}
       </button>
@@ -24,6 +33,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateIsComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
